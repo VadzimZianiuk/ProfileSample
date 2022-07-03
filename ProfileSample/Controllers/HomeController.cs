627507b7dc88e7d658bb3e91a36a9dbb583facd6
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2022-07-03 22:35:44 +0200

    refactored

--- ProfileSample/Controllers/HomeController.cs
+++ ProfileSample/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using ProfileSample.DAL;
+﻿using ProfileSample.DAL;
 using ProfileSample.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace ProfileSample.Controllers
 {
@@ -13,54 +10,21 @@ namespace ProfileSample.Controllers
     {
         public ActionResult Index()
         {
-            var context = new ProfileSampleEntities();
-
-            var sources = context.ImgSources.Take(20).Select(x => x.Id);
-            
-            var model = new List<ImageModel>();
-
-            foreach (var id in sources)
+            List<ImageModel> model;
+            using (var context = new ProfileSampleEntities())
             {
-                var item = context.ImgSources.Find(id);
-
-                var obj = new ImageModel()
+                model = context.ImgSources.Take(20).Select(x => new ImageModel()
                 {
-                    Name = item.Name,
-                    Data = item.Data
-                };
-
-                model.Add(obj);
-            } 
+                    Name = x.Name,
+                    Data = x.Data
+                }).ToList();
+            }
 
             return View(model);
         }
 
         public ActionResult Convert()
         {
-            var files = Directory.GetFiles(Server.MapPath("~/Content/Img"), "*.jpg");
-
-            using (var context = new ProfileSampleEntities())
-            {
-                foreach (var file in files)
-                {
-                    using (var stream = new FileStream(file, FileMode.Open))
-                    {
-                        byte[] buff = new byte[stream.Length];
-
-                        stream.Read(buff, 0, (int) stream.Length);
-
-                        var entity = new ImgSource()
-                        {
-                            Name = Path.GetFileName(file),
-                            Data = buff,
-                        };
-
-                        context.ImgSources.Add(entity);
-                        context.SaveChanges();
-                    }
-                } 
-            }
-
             return RedirectToAction("Index");
         }
 
